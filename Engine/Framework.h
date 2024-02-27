57af547b90bf13900a1aee3574393210f9c5d077
Mattéo <117280997+urrikki@users.noreply.github.com>
2024-02-27 17:30:35 +0100

    Add files via upload

---
+++ Engine/Framework.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <comdef.h>
+#include <fstream>
+#include <float.h>
+#include <cmath>
+#include <WindowsX.h>
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <DirectXMath.h>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#include <DirectXMath.h>
+using namespace DirectX;
+
+#include <windows.h>
